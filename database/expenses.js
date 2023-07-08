07d1ad4adc02b3d634beacd019eef21d9f26ab65
Micky Mannu <manishaacharya05@gmail.com>
2023-07-08 07:44:07 +0545

    day -2 nodejs

---
+++ database/expenses.js
@@ -0,0 +1,23 @@
+let expenses =[{
+    "_id":1,
+    "title":"food",
+    "paidBy":"sonika",
+    "description":"Lunch in resturant",
+    "amount":2000
+},
+{
+"_id":2,
+"title":"transport",
+"paidBy":"aayesha",
+"description":"compute fare from butwal",
+"amount":2000
+},
+{
+"_id":3,
+"title":"hotel",
+"paidBy":"suvu",
+"description":"hotel dreamland",
+"amount":10000
+},
+]
+module.exports= expenses
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var expenses = require("../database/expenses");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/add", function (req, res, next) {
+  console.log(expenses);
+  res.render("addExpense");
 });
+
+router.get("/", function (req, res, next) {
+  console.log(expenses);
+  res.render("index", { title: "ExpenseTracker", expenseList: expenses });
+});
+//post req
+router.post("/saveexpense", function (req, res, next) {
+  let formData = {
+    title: req.body.title,
+    paidBy: req.body.paidBy,
+    description: req.body.description,
+    amount: req.body.amount,
+  };
+  console.log(formData);
+  expenses.push({ ...formData, _id: expenses.length + 1 });
+  res.redirect("/");
+});
+router.get("/edit/:id", function (req, res) {
+ 
+  const expense=expenses.find(expense=>expense._id == req.params.id);
+  console.log(expense)
+  res.render("editExpense",{expense:expense});
+});
+
+router.get("/delete/:index", function (req, res, next) {
+  //  console.log(req.params.index)
+  expense = expenses.find((expense) => expense.id == req.params.id);
+  res.redirect("editExpense", { expense: expense });
+});
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   console.log(expenses)
+//   res.render('index', { title: 'Express' });
+// });
 // router.get('/wlit', function(req, res, next) {
 //   res.render('index', { title: 'wlit' });
 // });
 
-router.get('/wlit', function(req, res, next) {
-  res.render('index', { title:'wlit' ,Number: 10});
+router.get("/wlit", function (req, res, next) {
+  res.render("index", { title: "wlit", Number: 10 });
 });
+router.post('/saveEdited/:id', function(req,res){
 
-
+ let formData = {
+  title: req.body.title,
+  paidBy: req.body.paidBy,
+  description: req.body.description,
+  amount: req.body.amount,
+};
+  const index = expenses.findIndex((expense) =>{ return expense._id ==req.params.id});
+    expenses.splice(index,1,{_id:req.params.id, ...formData})
+    res.redirect('/')
+});
 module.exports = router;
